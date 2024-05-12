f0e64b1ed0ab1f5f4da0dc425e2ac605b1eb4a8a
VimHax <vimukthi.weerabahu@gmail.com>
2024-05-12 15:30:59 +0530

    Making landing responsive

--- app/sections/landing.tsx
+++ app/sections/landing.tsx
@@ -2,12 +2,13 @@ import Logo from '@/app/components/svg/Logo';
 
 import bg from './shader.fs';
 import Shader from '../components/shader/Shader';
+import MenuIcon from '../components/svg/MenuIcon';
 
 function Link({ name, url }: { name: string; url: string }) {
 	return (
 		<a
 			href={url}
-			className="flex h-full items-center justify-center px-6 font-semibold transition hover:bg-white hover:text-black"
+			className="hidden h-full items-center justify-center px-6 font-semibold transition hover:bg-white hover:text-black lg:flex"
 		>
 			{name}
 		</a>
@@ -20,7 +21,7 @@ export default function Landing() {
 			id="home"
 			className="relative flex h-[800px] max-h-screen w-full items-center justify-center"
 		>
-			<h1 className="whitespace-nowrap font-title text-9xl font-semibold leading-[0.75]">
+			<h1 className="whitespace-nowrap font-title text-5xl font-semibold leading-[0.75] sm:text-7xl sm:leading-[0.75] md:text-8xl md:leading-[0.75] lg:text-9xl lg:leading-[0.75]">
 				Every detail <br /> accounted for.
 			</h1>
 			<Shader
@@ -29,17 +30,22 @@ export default function Landing() {
 				source={bg}
 				uniforms={{ Light: 1 }}
 			/>
-			<div className="absolute left-0 top-0 mt-8 flex w-full justify-center">
-				<div className="mx-8 flex h-14 w-full max-w-screen-xl items-center justify-between">
+			<div className="absolute left-0 top-0 flex w-full justify-center">
+				<div className="mx-4 mt-4 flex h-14 w-full max-w-screen-xl items-center justify-between sm:mx-8 sm:mt-8">
 					<a
 						href="#home"
-						className="group flex h-full items-center justify-center gap-4 rounded-2xl bg-neutral-950/50 px-4 backdrop-blur-3xl hover:bg-white hover:text-black"
+						className="group flex h-full items-center justify-center gap-4 rounded-2xl bg-neutral-950/50 px-4 backdrop-blur-3xl transition-colors hover:bg-white hover:text-black"
 					>
-						<Logo className="h-6 w-6 [&_path]:fill-white group-hover:[&_path]:fill-black" />
-						<span className="-mb-1 font-title text-xl font-semibold">Vimukthi Weerabahu</span>
+						<Logo className="h-6 w-6 [&_path]:fill-white [&_path]:transition-colors group-hover:[&_path]:fill-black" />
+						<span className="auto -mb-1 hidden font-title text-xl font-semibold md:block">
+							Vimukthi Weerabahu
+						</span>
 					</a>
-					<span className="font-bold">A Full-Stack Developer.</span>
+					<span className="hidden font-bold sm:block">A Full-Stack Developer.</span>
 					<div className="flex h-full items-center overflow-clip rounded-2xl bg-neutral-950/50 backdrop-blur-3xl">
+						<button className="group flex aspect-square h-full items-center justify-center transition-colors hover:bg-white hover:text-white lg:hidden">
+							<MenuIcon className="h-7 w-7 [&_path]:fill-white [&_path]:transition-colors group-hover:[&_path]:fill-black" />
+						</button>
 						<Link name="Work" url="#work" />
 						<Link name="Skills" url="#skills" />
 						<Link name="Contact" url="#contact" />
