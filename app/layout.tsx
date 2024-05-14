7135c50b82a6237c51641cae1b7a28e12c0a771d
VimHax <vimukthi.weerabahu@gmail.com>
2024-05-14 18:52:27 +0530

    Update color

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 const titleFont = localFont({ src: './CabinetGrotesk-Variable.woff2', variable: '--font-title' });
 const bodyFont = localFont({ src: './Satoshi-Variable.woff2', variable: '--font-body' });
 
-export const viewport: Viewport = { themeColor: '#373DF8' };
+export const viewport: Viewport = { themeColor: '#EB237A' };
 
 export const metadata: Metadata = {
 	title: 'VimHax | Portfolio',
